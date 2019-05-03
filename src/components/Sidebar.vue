396c6ad77f17183e65697ce50327d7bfff2dd4d6
gkeele <gkeele@circlepix.com>
2019-05-03 15:46:19 -0600

    added authentication parts

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,38 @@
+<template>
+  <aside id="sidebar">
+    <!-- <i v-on:click="expandClick" class="fa fa-expand"></i> -->
+    <section id="logo">
+      <img alt="Topdawg logo" src="../assets/LogoSmall.png">
+    </section>
+    <section id="sidebarMenu">
+      <ul>
+        <li>
+          <i class="fa fa-user-secret"></i>
+          <router-link to="/admin">Admin</router-link>
+        </li>
+        <li>
+          <i class="fa fa-desktop"></i>
+          <router-link to="/">Dashboard</router-link>
+        </li>
+        <li>
+          <i class="fa fa-info-circle"></i>
+          <router-link to="/about">About</router-link>
+        </li>
+      </ul>
+    </section>
+  </aside>
+</template>
+
+<style src="../assets/css/main.css"></style>
+
+<script>
+export default {
+  name: "Sidebar",
+  methods: {
+    expandClick: function() {
+      alert("made it in");
+      document.getElementById("#sidebar").width = "50";
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,192 @@
 <template>
   <div class="home">
-    <the-header></the-header>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sidebar></Sidebar>
+
+    <MainHeader></MainHeader>
+
+    <div id="mainContent">
+      <div id="leftSide">
+        <div id="topRow">
+          <h3 class="gameHeading">Fantasy Golf</h3>
+          <img class="golfimage">
+        </div>
+        <div id="middleRow">
+          <div>
+            <h3 class="gameHeading">Bracket Challenge</h3>
+            <img class="golfimage">
+            <h3 class="gameHeading">Seed Challenge</h3>
+            <img class="golfimage">
+          </div>
+        </div>
+        <div id="bottomRow">
+          <img class="golfimage">
+          <img class="golfimage">
+          <img class="golfimage">
+          <img class="golfimage">
+        </div>
+      </div>
+      <div id="rightSide">
+        <h2>Informational</h2>
+      </div>
+      <!-- <div id="topSection">
+        <div id="marchMadness">
+          <h2>March Madness</h2>
+          <div id="bracketChallenge">
+            <h3>Bracket Challenge</h3>
+            <img>
+          </div>
+          <div id="seedChallenge">
+            <h3>Seed Challenge</h3>
+            <img>
+          </div>
+        </div>
+
+        <div id="golf">
+          <h2>Golf</h2>
+          <div id="phase1">
+            <h3>Golf</h3>
+            <img>
+          </div>
+        </div>
+      </div>
+
+      <div id="bottomSection">
+        <div id="fantasyFootball">
+          <h2>Football</h2>
+          <div id="headToHead">
+            <h3>Fantasy Football</h3>
+            <img>
+          </div>
+          <div id="salaryCap">
+            <h3>Salary Cap</h3>
+            <img>
+          </div>
+          <div id="pickem">
+            <h3>Pickem</h3>
+            <img>
+          </div>
+          <div id="loveEm">
+            <h3>Love Em &amp; Leave Em</h3>
+            <img>
+          </div>
+        </div>
+      </div>-->
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import TheHeader from '@/components/TheHeader.vue'
+import MainHeader from "@/components/MainHeader.vue";
+import Sidebar from "@/components/Sidebar.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld,
-    TheHeader
+    MainHeader,
+    Sidebar
   }
-}
+};
 </script>
+
+<style>
+#mainContent {
+  margin-left: 250px;
+  margin-top: 10px;
+  text-align: center;
+}
+
+#leftSide {
+  width: 50%;
+  float: left;
+  text-align: center;
+  border: 0px;
+}
+
+#rightSide {
+  width: 50%;
+  float: left;
+}
+
+#leftSide img {
+  height: 112px;
+  width: 112px;
+  border-style: none;
+}
+
+#middleRow > div {
+  display: inline;
+}
+
+.golfimage {
+  background: url(../assets/SportsSet2.png) 120px -10px;
+}
+
+/* #mainContent {
+  margin-left: 250px;
+  margin-top: 10px;
+  text-align: center;
+}
+
+#mainContent img {
+  height: 112px;
+  width: 112px;
+}
+
+#mainContent h2 {
+  color: white;
+}
+
+#bracketChallenge img {
+  background: url(../assets/SportsSet.png) -265px -10px;
+}
+#seedChallenge img {
+  background: url(../assets/SportsSet.png) -135px -10px;
+}
+
+#golf img {
+  background: url(../assets/SportsSet.png) 120px -10px;
+}
+
+#headToHead img {
+  background: url(../assets/FootballSet.png) 245px -10px;
+}
+#salaryCap img {
+  background: url(../assets/FootballSet.png) 120px -10px;
+}
+#pickem img {
+  background: url(../assets/FootballSet.png) -135px -10px;
+}
+#loveEm img {
+  background: url(../assets/FootballSet.png) -266px -10px;
+}
+
+#bottomSection {
+  margin-top: 20px;
+  overflow: hidden;
+}
+#topSection {
+  overflow: hidden;
+  text-align: center;
+}
+#bottomSection div,
+#topSection div {
+  background-color: #ab9869;
+  border-radius: 40px;
+  float: left;
+  margin: 0px 15px;
+  padding: 15px;
+}
+#bottomSection div div,
+#topSection div div {
+  cursor: pointer;
+}
+
+#fantasyFootball,
+#golf,
+#marchMadness {
+  background-color: #45597c !important;
+  border: thick solid black;
+  color: #2b1a0a;
+} */
+</style>
